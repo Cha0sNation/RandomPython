85a98a3dc57671be126826a9e0f559f0de573e5e
Thanas Pulaj <thanas2053@gmail.com>
2019-02-19 00:19:33 +0100

    Code style alterations

--- numberguess.py
+++ numberguess.py
@@ -3,18 +3,23 @@
 import random
 
 
-def check_guess(guess, number, tries, playing):
-    if guess == number:
+def check_guess():
+    global GUESS, NUMBER, TRIES, PLAYING
+    # pylint: disable=used-before-assignment
+    if GUESS == NUMBER:
         print("You guessed correctly.")
         print()
         again = input("Try again (y/n): ")
         print()
         if not again.lower().startswith("y"):
-            playing = False
+            PLAYING = False
+        else:
+            TRIES = 6
+            NUMBER = random.randint(1, 10)
     else:
         print("You guessed incorrectly, ", end="")
-        tries -= 1
-        if guess > number:
+        TRIES -= 1
+        if GUESS > NUMBER:
             print("your guess is too big.")
             print()
         else:
@@ -23,21 +28,22 @@ def check_guess(guess, number, tries, playing):
 
 
 if __name__ == "__main__":
-    number = random.randint(1, 10)
-    tries = 6
-    playing = True
-    while tries > 0 and playing:
+    NUMBER = random.randint(1, 10)
+    TRIES = 6
+    PLAYING = True
+    while TRIES > 0 and PLAYING:
         try:
-            guess = int(input("Guess a number (from 1-10): "))
+            GUESS = int(input("Guess a number (from 1-10): "))
         except (EOFError, KeyboardInterrupt):
             print()
             break
         except ValueError:
             print("Invalid number")
             break
-        check_guess(guess, number, tries, playing)
-        if tries == 0:
+        check_guess()
+        if TRIES == 0:
             print("You failed to guess correctly")
-            print("The correct number was {0}".format(number))
+            print("The correct number was {0}".format(NUMBER))
             break
-        print("You have {0} tries left".format(tries))
+        if PLAYING:
+            print("You have {0} tries left".format(TRIES))
--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@
 
 import random, sys
 
-template_board = {
+TEMPLATE_BOARD = {
     "top-L": " ",
     "top-M": " ",
     "top-R": " ",
@@ -14,7 +14,7 @@ template_board = {
     "bot-R": " ",
 }
 
-victory_seq = [
+VICTORY_SEQ = [
     ("top-L", "top-M", "top-R"),
     ("mid-L", "mid-M", "mid-R"),
     ("bot-L", "bot-M", "bot-R"),
@@ -26,7 +26,7 @@ victory_seq = [
 ]
 
 
-def print_board(board):
+def print_board(BOARD):
     print(
         """
          {0} | {1} | {2}
@@ -35,15 +35,15 @@ def print_board(board):
         -----------
          {6} | {7} | {8}
         """.format(
-            board["top-L"],
-            board["top-M"],
-            board["top-R"],
-            board["mid-L"],
-            board["mid-M"],
-            board["mid-R"],
-            board["bot-L"],
-            board["bot-M"],
-            board["bot-R"],
+            BOARD["top-L"],
+            BOARD["top-M"],
+            BOARD["top-R"],
+            BOARD["mid-L"],
+            BOARD["mid-M"],
+            BOARD["mid-R"],
+            BOARD["bot-L"],
+            BOARD["bot-M"],
+            BOARD["bot-R"],
         )
     )
 
@@ -51,13 +51,12 @@ def print_board(board):
 def get_symbol():
     while True:
         try:
-            playerSymbol = str(input("Do you want to be X or O ?: ")).upper()
-            if playerSymbol == "X":
-                return [playerSymbol, "O"]
-            elif playerSymbol == "O":
-                return [playerSymbol, "X"]
-            else:
-                raise ValueError
+            PLAYER_SYMBOL = str(input("Do you want to be X or O ?: ")).upper()
+            if PLAYER_SYMBOL == "X":
+                return [PLAYER_SYMBOL, "O"]
+            if PLAYER_SYMBOL == "O":
+                return [PLAYER_SYMBOL, "X"]
+            raise ValueError
         except (EOFError, KeyboardInterrupt):
             print()
             sys.exit()
@@ -68,18 +67,22 @@ def get_symbol():
 def get_first():
     if random.randint(0, 1):
         print("Player goes first")
+        print()
         return True
     print("Computer goes first")
+    print()
     return False
 
 
-def get_player_move(player_symbol, board):
+def get_player_move():
+    global PLAYER_SYMBOL, BOARD
     while True:
         try:
-            player_move = input("Your move (top/mid/bot - L/M/R):")
-            if player_move in board and board[player_move] == " ":
-                board[player_move] = player_symbol
-                print_board(board)
+            player_move = input("Your move (top/mid/bot - L/M/R): ")
+            if player_move in BOARD and BOARD[player_move] == " ":
+                BOARD[player_move] = PLAYER_SYMBOL
+                print("Player's move: ")
+                print_board(BOARD)
                 break
             else:
                 print("Invalid move")
@@ -89,124 +92,110 @@ def get_player_move(player_symbol, board):
         except ValueError:
             print("Invalid choice")
 
-    if not check_finished(board, victory_seq):
+    if not check_finished():
         return False
-    else:
+    return True
+
+
+def get_computer_move():
+    global COMPUTER_SYMBOL, PLAYER_SYMBOL, BOARD, VICTORY_SEQ
+    print("Computer's move: ")
+    for condition in VICTORY_SEQ:
+        if (
+            BOARD[condition[0]] == BOARD[condition[1]] == COMPUTER_SYMBOL
+            and BOARD[condition[2]] != PLAYER_SYMBOL
+        ):
+            BOARD[condition[2]] = COMPUTER_SYMBOL
+            print_board(BOARD)
+            if not check_finished():
+                return False
+            return True
+        if (
+            BOARD[condition[0]] == BOARD[condition[2]] == COMPUTER_SYMBOL
+            and BOARD[condition[1]] != PLAYER_SYMBOL
+        ):
+            BOARD[condition[1]] = COMPUTER_SYMBOL
+            print_board(BOARD)
+            if not check_finished():
+                return False
+            return True
+        if (
+            BOARD[condition[1]] == BOARD[condition[2]] == COMPUTER_SYMBOL
+            and BOARD[condition[0]] != PLAYER_SYMBOL
+        ):
+            BOARD[condition[0]] = COMPUTER_SYMBOL
+            print_board(BOARD)
+            if not check_finished():
+                return False
+            return True
+
+    for condition in VICTORY_SEQ:
+        if (
+            BOARD[condition[0]] == BOARD[condition[1]] == PLAYER_SYMBOL
+            and BOARD[condition[2]] != COMPUTER_SYMBOL
+        ):
+            BOARD[condition[2]] = COMPUTER_SYMBOL
+            print_board(BOARD)
+            if not check_finished():
+                return False
+            return True
+        if (
+            BOARD[condition[0]] == BOARD[condition[2]] == PLAYER_SYMBOL
+            and BOARD[condition[1]] != COMPUTER_SYMBOL
+        ):
+            BOARD[condition[1]] = COMPUTER_SYMBOL
+            print_board(BOARD)
+            if not check_finished():
+                return False
+            return True
+        if (
+            BOARD[condition[1]] == BOARD[condition[2]] == PLAYER_SYMBOL
+            and BOARD[condition[0]] != COMPUTER_SYMBOL
+        ):
+            BOARD[condition[0]] = COMPUTER_SYMBOL
+            print_board(BOARD)
+            if not check_finished():
+                return False
+            return True
+
+    for key in BOARD.keys():
+        if key in ("top-L", "top-R", "bot-L", "bot-R") and BOARD[key] == " ":
+            BOARD[key] = COMPUTER_SYMBOL
+            print_board(BOARD)
+            if not check_finished():
+                return False
+            return True
+
+    if BOARD["mid-M"] == " ":
+        BOARD["mid-M"] = COMPUTER_SYMBOL
+        print_board(BOARD)
+        if not check_finished():
+            return False
         return True
 
-
-def get_computer_move(computer_symbol, player_symbol, board, victory_seq):
-
-    for condition in victory_seq:
-        if (
-            board[condition[0]] == board[condition[1]] == computer_symbol
-            and board[condition[2]] != player_symbol
-        ):
-            board[condition[2]] = computer_symbol
-            print_board(board)
-            if not check_finished(board, victory_seq):
+    for key in BOARD.keys():
+        if key in ("top-M", "mid-L", "mid-R", "bot-M") and BOARD[key] == " ":
+            BOARD[key] = COMPUTER_SYMBOL
+            print_board(BOARD)
+            if not check_finished():
                 return False
-            else:
-                return True
-        elif (
-            board[condition[0]] == board[condition[2]] == computer_symbol
-            and board[condition[1]] != player_symbol
-        ):
-            board[condition[1]] = computer_symbol
-            print_board(board)
-            if not check_finished(board, victory_seq):
-                return False
-            else:
-                return True
-        elif (
-            board[condition[1]] == board[condition[2]] == computer_symbol
-            and board[condition[0]] != player_symbol
-        ):
-            board[condition[0]] = computer_symbol
-            print_board(board)
-            if not check_finished(board, victory_seq):
-                return False
-            else:
-                return True
-
-    for condition in victory_seq:
-        if (
-            board[condition[0]] == board[condition[1]] == player_symbol
-            and board[condition[2]] != computer_symbol
-        ):
-            board[condition[2]] = computer_symbol
-            print_board(board)
-            if not check_finished(board, victory_seq):
-                return False
-            else:
-                return True
-        elif (
-            board[condition[0]] == board[condition[2]] == player_symbol
-            and board[condition[1]] != computer_symbol
-        ):
-            board[condition[1]] = computer_symbol
-            print_board(board)
-            if not check_finished(board, victory_seq):
-                return False
-            else:
-                return True
-        elif (
-            board[condition[1]] == board[condition[2]] == player_symbol
-            and board[condition[0]] != computer_symbol
-        ):
-            board[condition[0]] = computer_symbol
-            print_board(board)
-            if not check_finished(board, victory_seq):
-                return False
-            else:
-                return True
-
-    for key in board.keys():
-        if (
-            key == "top-L" or key == "top-R" or key == "bot-L" or key == "bot-R"
-        ) and board[key] == " ":
-            board[key] = computer_symbol
-            print_board(board)
-            if not check_finished(board, victory_seq):
-                return False
-            else:
-                return True
-        else:
-            continue
-    if board["mid-M"] == " ":
-        board["mid-M"] = computer_symbol
-        print_board(board)
-        if not check_finished(board, victory_seq):
-            return False
-        else:
             return True
-    for key in board.keys():
-        if (
-            key == "top-M" or key == "mid-L" or key == "mid-R" or key == "bot-M"
-        ) and board[key] == " ":
-            board[key] = computer_symbol
-            print_board(board)
-            if not check_finished(board, victory_seq):
-                return False
-            else:
-                return True
-        else:
-            continue
 
 
-def check_finished(board, victory_seq):
-    for condition in victory_seq:
+def check_finished():
+    global BOARD, VICTORY_SEQ
+    for condition in VICTORY_SEQ:
         if (
-            board[condition[0]] == board[condition[1]] == board[condition[2]] == "X"
-        ) or (board[condition[0]] == board[condition[1]] == board[condition[2]] == "O"):
-            if board[condition[0]] == "O":
+            BOARD[condition[0]] == BOARD[condition[1]] == BOARD[condition[2]] == "X"
+        ) or (BOARD[condition[0]] == BOARD[condition[1]] == BOARD[condition[2]] == "O"):
+            if BOARD[condition[0]] == "O":
                 print("The computer won")
             else:
                 print("You won")
-            board[condition[0]] = board[condition[1]] = board[condition[2]] = "*"
-            print_board(board)
+            BOARD[condition[0]] = BOARD[condition[1]] = BOARD[condition[2]] = "*"
+            print_board(BOARD)
             return True
-    for cell in board.values():
+    for cell in BOARD.values():
         if cell == " ":
             return False
     print("It's a tie")
@@ -214,16 +203,14 @@ def check_finished(board, victory_seq):
 
 
 if __name__ == "__main__":
-    print_board(template_board)
-    board = template_board.copy()
-    player_symbol, computer_symbol = get_symbol()
+    print_board(TEMPLATE_BOARD)
+    BOARD = TEMPLATE_BOARD.copy()
+    PLAYER_SYMBOL, COMPUTER_SYMBOL = get_symbol()
     if get_first():
         while True:
             # if not finished
-            if not get_player_move(player_symbol, board):
-                if not get_computer_move(
-                    computer_symbol, player_symbol, board, victory_seq
-                ):
+            if not get_player_move():
+                if not get_computer_move():
                     continue
                 else:
                     break
@@ -232,10 +219,8 @@ if __name__ == "__main__":
     else:
         while True:
             # if not finished
-            if not get_computer_move(
-                computer_symbol, player_symbol, board, victory_seq
-            ):
-                if not get_player_move(player_symbol, board):
+            if not get_computer_move():
+                if not get_player_move():
                     continue
                 else:
                     break
